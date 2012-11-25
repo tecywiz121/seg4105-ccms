01393b01fc28535529aee44033cbe0d61478a5ee
Sam Wilson <tecywiz121@hotmail.com>
2012-11-25 18:03:21 -0500

    Set up static directories

--- ccms/settings.py
+++ ccms/settings.py
@@ -49,7 +49,7 @@ USE_TZ = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
-MEDIA_ROOT = p_join(BASE_DIR, 'media')
+MEDIA_ROOT = p_join(BASE_DIR, 'media/')
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
@@ -60,7 +60,7 @@ MEDIA_URL = '/media/'
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = p_join(BASE_DIR, 'static')
+STATIC_ROOT = p_join(BASE_DIR, 'static/')
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
@@ -71,6 +71,7 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    p_join(BASE_DIR, 'extra_static/'),
 )
 
 # List of finder classes that know how to find static files in
